f23cb806475b8ce932d6c2656d21a632fa30035c
Abhijit Roy <dalvath3700@gmail.com>
2019-03-27 03:06:15 +0530

    added scripts, organized

---
+++ src/dir1.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+rem All the directory listings from C:\Users\Abhijit\Desktop will be routed to the file list.txt
+rem copy all the files, folders inside Desktop folder into a file -- "list.txt"
+dir C:\Users\Abhijit\Desktop > C:\Users\Abhijit\Desktop\list.txt
+
+rem open the .txt file using notepad
+notepad C:\Users\Abhijit\Desktop\list.txt
