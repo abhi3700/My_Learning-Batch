5c0889bbfbc7bc8fc7d01bcfda5c9e263dfabe12
Abhijit Roy <dalvath3700@gmail.com>
2019-04-20 12:39:17 +0530

    Show/Hide files, folders

---
+++ src/hidefilefolder.bat
@@ -0,0 +1,6 @@
+@echo off
+rem -------------------------------------------------------------------
+rem This hides the file, folders
+rem NOTE: check the file if exist, then only run
+rem -------------------------------------------------------------------
+attrib +r +h +s Book1.xlsx
---
+++ src/showfilefolder.bat
@@ -0,0 +1,6 @@
+@echo off
+rem -------------------------------------------------------------------
+rem This shows the hidden file, folders
+rem NOTE: check the file if exist, then only run
+rem -------------------------------------------------------------------
+attrib -r -h -s Book1.xlsx
